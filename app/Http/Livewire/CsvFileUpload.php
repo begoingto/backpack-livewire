83236ca9dab835b232459bf5924f5f31c82822ff
MichBK <mich.neuy@bi-kay.com>
2023-01-14 11:24:36 +0700

    add upload livewire component

--- app/Http/Livewire/CsvFileUpload.php
+++ app/Http/Livewire/CsvFileUpload.php
@@ -1,11 +1,43 @@
 <?php
-
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class CsvFileUpload extends Component
 {
+    use WithFileUploads;
+
+    public bool $open = false;
+    public $file;
+    public string $model;
+
+    protected $listeners = [
+        'toggle'
+    ];
+
+    protected function rules()
+    {
+        return [
+            'file' => ['required', 'mimes:csv', 'max:51200']
+        ];
+    }
+
+    public function updatedFile()
+    {
+        //validation
+        $this->validateOnly('file');
+
+        // read csv
+
+        // grab data from csv
+    }
+
+    public function toggle()
+    {
+        $this->open = !$this->open;
+    }
+
     public function render()
     {
         return view('livewire.csv-file-upload');
--- resources/views/custom/category-csv-upload.blade.php
+++ resources/views/custom/category-csv-upload.blade.php
@@ -13,40 +13,7 @@
 
 
 @section('content')
-    <div class="drag-drop w-100 vh-75 d-flex flex-column justify-content-center p-4 align-items-center">
-        <h1><i class="la la-cloud-upload la-lg mt-4"></i></h1>
-        <h3>Drag and Drop</h3>
-        <h3>Or <label for="upload" class="text-primary">Browse</label></h3>
-    </div>
-    <hr>
-    <div class="card">
-        <div class="card-body d-flex">
-            <h1 class="text-primary">
-                <i class="la la-cloud-upload la-lg mt-4"></i>
-            </h1>
-            <div class="ml-3 w-100">
-                <p>Category_template.csv</p>
-                <div class="progress">
-                    <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" style="width: 80%;" aria-valuenow="80" aria-valuemin="0"
-                        aria-valuemax="100">80%</div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="card">
-        <div class="card-body d-flex">
-            <h1 class="text-primary">
-                <i class="la la-cloud-upload la-lg mt-4"></i>
-            </h1>
-            <div class="ml-3 w-100">
-                <p>Category_template.csv</p>
-                <div class="progress">
-                    <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" style="width: 80%;" aria-valuenow="80" aria-valuemin="0"
-                        aria-valuemax="100">80%</div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <livewire:csv-file-upload />
 @endsection
 
 @push('after_styles')
@@ -59,4 +26,10 @@
             cursor: pointer;
         }
     </style>
+    @livewireStyles
+    <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
+@endpush
+
+@push('after_scripts')
+    @livewireScripts
 @endpush
